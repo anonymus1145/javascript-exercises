20fd3089bbb8c1ff779c48187cd4c5f6d52816f4
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-02 16:52:02 +0300

    Finish exercises 03
    To convert any strings and pass the tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(strToReverse) {
+    let strReverse = "";
+    const myArray = strToReverse.split("");
 
+    for (let i = myArray.length - 1; i >= 0; i--) {
+        strReverse += myArray[i];
+    }
+    return strReverse
 };
 
 // Do not edit below this line
