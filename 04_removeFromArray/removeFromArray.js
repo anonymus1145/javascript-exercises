49fa7a661f090a84d16c2f371087c5dc009b0b41
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-02 20:46:13 +0300

    Finish task 04
    To complete the task and pass the tests
    
    [Ticket: X]

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (myArray, ...extras) {
 
+    let elements = [];
+
+    for (let y = 0; y < myArray.length; y++) {
+
+        elements.push(myArray[y])
+
+        for (let i = 0; i < extras.length; i++) {
+            if (extras[i] === myArray[y]) {
+                elements.pop(extras[i])
+            }
+        }
+    }
+    return elements;
 };
 
 // Do not edit below this line
