3d975a3991f0a12527eb6cc17cffe305069f8739
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-02 15:55:32 +0300

    Finish exercises 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (strarguent, numargument) {
 
+    let streturn = "";
+
+    if (numargument < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < numargument; i++) {
+        streturn += strarguent;
+    }
+    return streturn;
 };
 
 // Do not edit below this line
