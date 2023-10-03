4a86e6f01d215a7d2178e1c53c36f18de626a364
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-03 17:18:56 +0300

    Finish task 05
    To pass the tests
    
    [Ticket: X]

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    //const count = [];
+    let sum = 0;
 
+    if (Number.isInteger(firstInt) == false || Number.isInteger(secondInt) == false) {
+        return "ERROR";
+    } else if (firstInt < 0 || secondInt < 0) {
+        return "ERROR";
+    } else if (firstInt < secondInt) {
+        for (let i = firstInt; i <= secondInt; i++) {
+            //count.push(i);
+            sum += i;
+        }
+    } else {
+        for (let i = secondInt; i <= firstInt; i++) {
+            //count.push(i);
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+//let number = sumAll(1,100);
+//console.log(number);
