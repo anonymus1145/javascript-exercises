f2aae05a10d31658e154daac84e17a41dec88eec
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-03 20:55:58 +0300

    Finish first part of the tasks 01->07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(intFahrenheit) {
+  return Math.round((intFahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(intCelsius) {
+  return Math.round((((intCelsius * 9) / 5) + 32)* 10) / 10;
 };
 
 // Do not edit below this line
