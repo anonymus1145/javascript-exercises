19472ea8e0fcc89313e17f74cb164b790d3018b2
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-06 18:04:18 +0300

    Implement palindrome checker function
    Added the `palindromes` function to check if a given word is a palindrome. The function removes specified characters (such as '!', '.', ',', '?', and space) from the word and checks if the resulting word is the same when read in reverse.
    
    The function uses the following steps:
    1. Splits the input word into an array of characters.
    2. Filters out the specified characters using the `filter()` method.
    3. Joins the filtered array back into a string.
    4. Creates a reversed version of the filtered string using the `reverse()` method.
    5. Compares the lowercase versions of the original and reversed strings for equality.
    6. Returns `true` if the word is a palindrome and `false` otherwise.
    
    The implementation has been tested and returns the expected results for different palindromic and non-palindromic words.
    
    Fixes 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const valueToRemove = ['!', '.', ',','?',' '];
+    let arr = word.split('');
+    let newWord = '';
+
+    for (let i = 0; i < valueToRemove.length; i++) {
+       arr = arr.filter((char) => char !== valueToRemove[i]);
+    }
+
+    newWord = arr.join('');
+    let arr2 = arr.reverse();
+    let arr3 = arr2.join('');
+    if (newWord.toLowerCase() === arr3.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
