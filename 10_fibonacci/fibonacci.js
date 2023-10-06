89bcf29f2371c38485bae627d4f93fff96c1a227
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-06 20:21:25 +0300

    Implement Fibonacci function
    This commit adds the implementation of the Fibonacci function.
    The function takes a number as an argument and returns the Fibonacci
    number at that position.
    It handles negative numbers by returning the string "OOPS".
    The function calculates the Fibonacci sequence using a loop and an
    array. It then slices the array to get the Fibonacci number at the
    specified position and returns it.
    
    Fixes 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if (number < 0) {
+        return "OOPS";
+    } else {
+        let fibSeq = [0, 1];
 
-};
+        for (let i = 2; i <= number; i++) {
+            fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2];
+        }
+        let fibNum = fibSeq.slice(0, number + 1);
+
+        let finalNum = fibNum.pop();
+
+        return finalNum;
+    };
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
