d3b3c0faf819f02b50905f38b5b15faf2d30814a
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-06 16:48:21 +0300

    Implement mathematical functions
    add, subtract, get the sum, multiply, get the power, and find the
    factorial
    
    [Fixes 08_calculator]

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (first, second) {
+  let sum = first + second;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (first, second) {
+  let dif = first - second;
+  return dif;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let rez;
+  if (arr.length === 0) {
+    rez = 0;
+  } else {
+    rez = arr.reduce((sum, current) => sum + current, 0);
+  }
+  return parseInt(rez);
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let result = 1;
+  arr.forEach(element => {
+    result = result * element;
+  });
 
+return result;
 };
 
-const power = function() {
-	
+const power = function (first, second) {
+  return Math.pow(first, second);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let product = 1;
+  for (let i = 2; i <= num; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
