7c107dc3e046d69b2fc6bf7d272bfeaa5560d4c0
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-08 15:44:45 +0300

    Function to find an element from an array of objects -> 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let age = 0;
+    let objAges = [];
+    let person;
 
+    for (let item of array)
+        if (item.yearOfBirth < item.yearOfDeath) {
+            age = item.yearOfDeath - item.yearOfBirth;
+            objAges.push(age);
+        } else if (item.yearOfDeath == null) {
+            age = 2023 - item.yearOfBirth;
+            objAges.push(age);
+        }
+    for (let i = 0; i < objAges.length; i++) {
+        if (objAges[i] > age) {
+            age = objAges[i];
+            person = array[i];
+        }
+    }
+    return person;
 };
 
 // Do not edit below this line
